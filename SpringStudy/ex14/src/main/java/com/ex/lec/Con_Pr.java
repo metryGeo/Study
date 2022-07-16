3048200e7372dcdee124cac3e8ff3d9406a7fb8a
Gihak Yun <adad0207@naver.com>
2022-07-16 17:01:09 +0900

    Sts 를 이용한 Spring_MVC 웹 프로젝트 연습 1

---
+++ SpringStudy/ex14/src/main/java/com/ex/lec/Con_Pr.java
@@ -0,0 +1,40 @@
+package com.ex.lec;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Con_Pr extends HttpServlet {
+	
+	@RequestMapping("/homee")
+	public String home(Model model) {	
+		model.addAttribute("name_Key", "name_Value");
+		return "home";
+	}
+	
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ SpringStudy/ex14/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,14 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<h1>
+	Hello world!  
+</h1>
+
+<P>  THis is ${name_Key}. </P>
+</body>
+</html>
