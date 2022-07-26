a8c1e22f8e75c982e9d361981ac3427de8831cc1
Gihak Yun <adad0207@naver.com>
2022-07-26 18:47:48 +0900

    Mybatis 까지 Spring

---
+++ SpringStudy/Book/ex00/src/main/java/org/zerock/jdbc/JdbcTest.java
@@ -0,0 +1,39 @@
+package org.zerock.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.jsp.tagext.TryCatchFinally;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class JdbcTest {
+
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testConnection() {
+		
+		try (Connection con =
+				DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE",
+						"c##scott",
+						"tiger"))
+		{
+			log.info(con);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
