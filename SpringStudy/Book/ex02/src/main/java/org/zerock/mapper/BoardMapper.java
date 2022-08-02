330956226be1cd95f0accedf16bc87bfb219f7b9
Gihak Yun <adad0207@naver.com>
2022-08-02 22:37:22 +0900

    mybatis와 mapper를 이용한 CRUD 구현

--- SpringStudy/Book/ex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ SpringStudy/Book/ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -10,4 +10,13 @@ public interface BoardMapper {
 //	@Select("select * from tbl_board where bno > 0")
 	public List<BoardVO> getList();
 
+	public void insert(BoardVO vo);
+
+	public void insertSelectKey(BoardVO vo);
+
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+
+	public int update(BoardVO vo);
 }
--- SpringStudy/Book/ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ SpringStudy/Book/ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -1,11 +1,11 @@
 package org.zerock.mapper;
 
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -16,9 +16,60 @@ import lombok.extern.log4j.Log4j;
 public class BoardMapperTests {
 	@Setter(onMethod_ = { @Autowired })
 	private BoardMapper mapper;
-	@Test
+
+//	@Test
 	public void testgetList() {
 		mapper.getList().forEach(board -> log.info(board));
 	}
 	
+//	@Test
+	public void testinsert() {
+		BoardVO vo = new BoardVO();
+		vo.setTitle("Spring_Title");
+		vo.setContent("Spring_Content");
+		vo.setWriter("Spring_Writer");
+		
+		mapper.insert(vo);
+		log.info(vo);
+	}
+	
+//	@Test
+	public void testInsertSelectKey() {
+		BoardVO vo = new BoardVO();
+		vo.setTitle("Spring_select_Title");
+		vo.setContent("Spring_select_Content");
+		vo.setWriter("Spring_select_Writer");
+		
+		mapper.insertSelectKey(vo);
+		log.info(vo);
+	}
+	
+//	@Test
+	public void testRead() {
+		BoardVO vo = mapper.read((long)21);
+		
+		log.info(vo);
+		
+	}
+	
+//	@Test
+	public void testDelete() {
+		mapper.delete((long)21);
+		
+	}
+	
+	@Test
+	public void testUpdate() {
+		BoardVO vo = new BoardVO();
+		vo.setBno((long)22);
+		vo.setTitle("updated_Title");
+		vo.setContent("updated_Content");
+		vo.setWriter("updated_Writer");
+		
+		int count = mapper.update(vo);
+		
+		log.info("update Count : " + count);
+	}
+	
+
 }
